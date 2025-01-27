aff44f815ca8c46d5359bed304b9fb0ca8a51040
Mansoor <shaikmansoor1200@gmail.com>
2025-01-27 14:01:57 +0530

    edited db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect(process.env.DATABASE_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
