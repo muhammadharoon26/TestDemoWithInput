cc2e57c4cff3c85681c986dc76f1dc590a927663
Muhammad Haroon <mharoon1326@gmail.com>
2025-03-14 07:20:40 +0500

    Set up CI/CD pipeline with GitHub Actions

--- backend/app.js
+++ backend/app.js
@@ -51,7 +51,7 @@ const healthRoutes = require('./routes/healthRoutes'); // <-- Added
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
