6498f407b44a0e7c944633826a14ef1523b8b926
Muhammad Haroon <mharoon1326@gmail.com>
2025-03-14 11:01:31 +0500

    Renamed app.js to server.js and updated references

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,9 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 });
-
+app.get('/',(req, res) => {
+  res.send('Hello!')
+})
 app.use('/api/data', dataRoutes);
 app.use('/health', healthRoutes); // <-- Added health check route
 
